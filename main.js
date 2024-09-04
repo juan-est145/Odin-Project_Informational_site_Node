165f229838d1cdea53a78b4af135159d84268f34
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-09-04 09:45:49 +0200

    We can parse the url of the requests

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const http = require("http");
 const eventEmitter = require("node:events");
+const url = require("node:url");
 
 const server = http.createServer((req, res) => {
 	res.writeHead(200, { "Content-Type": "text/html" });
 	res.write(req.url);
-	res.write("\n");
+	const urlObject = url.parse(req.url, true);
+	const dateParam = urlObject.query.date;
+	if (dateParam) {
+		const date = new Date(dateParam);
+		res.write(`Date parameter ${date.toISOString()}\n`);
+	} else {
+		res.write("No date parameter provided\n");
+	}
 	res.end("Hello world");
 });
 server.listen(8080);
@@ -14,4 +22,3 @@ startEvent.on("start", () => {
 	console.log("Server has been started");
 });
 startEvent.emit("start");
-
