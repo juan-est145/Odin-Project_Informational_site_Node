61384379768d072aaa206e99f95ca04c7be5f623
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-09-04 09:22:43 +0200

    We print the request url

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 const http = require("http");
 const eventEmitter = require("node:events");
 
+const server = http.createServer((req, res) => {
+	res.writeHead(200, { "Content-Type": "text/html" });
+	res.write(req.url);
+	res.write("\n");
+	res.end("Hello world");
+});
+server.listen(8080);
+
 const startEvent = new eventEmitter();
 startEvent.on("start", () => {
 	console.log("Server has been started");
 });
 startEvent.emit("start");
-
-const server = http.createServer((req, res) => {
-	res.writeHead(200, { "Content-Type": "text/html" });
-	res.end("Hello world");
-});
-server.listen(8080);
 
